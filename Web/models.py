8eed5110f0a6a4ef81b0ef55988cfe4de07fe718
iamdamified <119352851+iamdamified@users.noreply.github.com>
2025-03-06 16:06:05 +0100

    first model

--- Web/models.py
+++ Web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Hero(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='hero_images')
+
+    class Meta:
+        verbose_name_plural = 'Heroes'
+
+    def __str__(self):
+        return self.title
+    
+
+
