228d2cea2e503afde3e7198209476d9396e07d60
iamdamified <119352851+iamdamified@users.noreply.github.com>
2025-03-07 16:41:08 +0100

    firstview

--- ECOMMERCE/urls.py
+++ ECOMMERCE/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Web.urls')),
-    path('api/', include('Api.urls'))
+    path('', include('Web.urls'))
+    # path('api/', include('Api.urls'))
 ]
 
 
--- Web/views.py
+++ Web/views.py
@@ -13,10 +13,23 @@ def homepage(request):
         kids_products = Product.objects.filter(category='KD')
         social_images = Social.objects.all()
 
+        context = {
+            'categories': categories,
+            'men_products': men_products,
+            'women_products': wommen_products,
+            'kids_products':kids_products,
+            'social_images': social_images
+        }
+    else:
+        subscriber_name = request.POST.get('name')
+        subscriber_email = request.POST.get('email')
+        subscriber = Subscriber(name=subscriber_name, email=subscriber_email)
+        subscriber.save()
+
+    return render(request, 'web/index.html', context)
 
 
+# def products(request):
 
-def products(request):
 
-
-def single_product(request):
+# def single_product(request):
